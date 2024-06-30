92ea0a7e1e9e7e737137f0941fae610f4b0ffda2
Gideon Mutai <giddymutai13@gmail.com>
2024-06-30 14:22:45 +0300

    finish routing lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    return f'{parameter}'
+
+@app.route('/print/<int:parameter>')
+def count(parameter):
+    if parameter <= 0:
+        return 'Please enter a positive integer'
+    
+    output = "\n".join(x for x in range(1, parameter + 1))
+    return output
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    operations = {
+      "+": lambda a, b: a + b,
+      "-": lambda a, b: a - b,
+      "*": lambda a, b: a * b,
+      "div": lambda a, b: a / b, 
+      "%": lambda a, b: a % b,
+  }
+    
+    try:
+        result = operations[operation](num1, num2)
+        return f"{num1} {operation} {num2} = {result}"
+    except ZeroDivisionError:
+        return "Error: Division by zero"
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
